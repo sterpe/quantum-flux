d77b9fe17d6b5f8dccad1c969f0b4ac2e1568215
Steve Terpe <sterpe@rubiconproject.com>
2014-07-25 19:26:11 -0700

    initial commit

---
+++ flux-dispatcher.js
@@ -0,0 +1,102 @@
+/*
+ * 
+ * flux-dispatcher.js */
+
+var _ = require('lodash'),
+  EventEmitter = require('./flux-events.js').EventEmitter,
+  TrampolineQueue = require('./flux-trampoline.js').TrampolineQueue,
+  Q = require('q');
+
+function Dispatcher() {
+  "use strict";
+  this._queue = this._queue || new TrampolineQueue();
+  this._emitter = this._emitter || new EventEmitter();
+}
+
+_.extend(Dispatcher.prototype, {
+
+  register: function (listener) {
+    "use strict";
+    var i;
+
+    Dispatcher.prototype.constructor.call(this);
+
+    i = EventEmitter.listenerCount(this._emitter, 'dispatch');
+
+    this._emitter.addListener('dispatch', _.bind(function (i, e) {
+      try {
+        listener.call(undefined, e);
+        this._deferreds[i].resolve(e);
+      } catch (r) {
+        this._deferreds[i].reject(r);
+      }
+    }, this, i));
+
+    if (EventEmitter.listenerCount(this._emitter, 'dispatch') !== (i + 1)) {
+      throw new Error();
+    }
+    return i;
+  },
+
+  /*
+  unregister: function (listener) {
+    "use strict";
+    if (this._emitter) {
+      this._emitter.removeListener('dispatch', listener);
+    }
+    return this;
+  },
+  */
+
+  dispatch: function (dispatch) {
+    "use strict";
+
+    Dispatcher.prototype.constructor.call(this);
+
+    this._queue.push(_.bind(function () {
+      this._deferreds = _.map(this._emitter.listeners('dispatch'),
+        function (_, i) {
+          return Q.defer();
+        });
+      this._emitter.emit('dispatch', dispatch);
+    }, this));
+  },
+
+  waitFor: function (storeIndex, callback, errback) {
+    "use strict";
+
+    if (_.isArray(storeIndex)) {
+      return Q.all(_.filter(this._deferreds, function (_, i) {
+        return storeIndex.indexOf(i) !== -1;
+      }));
+    }
+    return this._deferreds[storeIndex].promise.then(callback, errback);
+  }
+});
+
+module.exports = {
+  Dispatcher: Dispatcher
+};
+
+var dispatcher = new Dispatcher();
+
+//console.log(dispatcher);
+var index  = 0;
+dispatcher.register(function (e) {
+  dispatcher.waitFor(1, function () {
+    console.log(index++, 1, e);
+  });
+  setTimeout(function () {
+    dispatcher.dispatch(e);
+  }, 2000);
+});
+
+dispatcher.register(function (e) {
+  console.log(index++, 2, e);
+});
+
+//console.log(dispatcher);
+dispatcher.dispatch('\n');
+dispatcher.dispatch('foo');
+//dispatcher.dispatch('bar');
+//dispatcher.dispatch('baz');
---
+++ flux-trampoline.js
@@ -0,0 +1,97 @@
+/*
+ * 
+ * flux-trampoline.js */
+var _ = require('lodash'),
+  STATUS = {
+    IDLE: 0,
+    RUN: 1
+  };
+
+function TrampolineQueue() {
+  "use strict";
+  this._status = STATUS.IDLE;
+  this._queue = this._queue || (_.bind(function () {
+    this._onmessage = _.bind(this._onmessage, this);
+    this._nextTick = (function (self) {
+
+      var proc = _.bind(self._process, self);
+
+      if (typeof window !== "undefined") {
+        if (window.postMessage &&
+            _.isFunction(window.postMessage)) {
+          return function () {
+            window.addEventListener('message', this._onmessage, false);
+            window.postMessage(this._key, "*");
+          };
+        }
+        return function () {
+          setTimeout(proc, 0);
+        };
+      }
+      return function () {
+        process.nextTick(proc);
+      };
+    }(this));
+
+    this._key = "flux-dispatch:" + this._keygen();
+
+    return [];
+
+  }, this)());
+
+}
+
+_.extend(TrampolineQueue.prototype, {
+
+  push: function (task) {
+    "use strict";
+
+    TrampolineQueue.prototype.constructor.call(this);
+
+    if (this._status === STATUS.IDLE) {
+      this._status = STATUS.RUN;
+      this._nextTick();
+    }
+    return this._queue.push(task);
+  },
+
+  _keygen: function () {
+    "use strict";
+
+    return (Math.random() * 1E17)
+      .toString(Math.floor(Math.random() * 35) + 2);
+  },
+
+  _onmessage: function (e) {
+    "use strict";
+
+    if (!e || e.source !== window ||
+        e.data !== this._key) {
+      return;
+    }
+    this._process();
+    window.removeEventListener('message', this._onmessage, false);
+    this._key = "flux-dispatch:" + this._keygen();
+  },
+
+  _process: function () {
+    "use strict";
+
+    var work_queue = [];
+
+    while (this._queue.length) {
+      while (this._queue.length) {
+        work_queue.push(this._queue.pop());
+      }
+      while (work_queue.length) {
+        work_queue.pop().call();
+      }
+    }
+
+    this._status = STATUS.IDLE;
+  }
+});
+
+module.exports = {
+  TrampolineQueue: TrampolineQueue
+};
