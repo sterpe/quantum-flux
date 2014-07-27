dd6823b45f4d2393b66c9b5e1157c8e8f5391a85
Steve Terpe <sterpe@rubiconproject.com>
2014-07-27 15:00:03 -0700

    refactor so that dispatcher has a specific Store constructor tied to it

--- flux.js
+++ flux.js
@@ -2,9 +2,27 @@
  * 
  * flux.js */
 
-module.exports = {
-  Dispatcher: require('./lib/flux-dispatcher.js').Dispatcher,
-  EventEmitter: require('./lib/flux-events.js').EventEmitter,
-  Store: require('./lib/flux-store.js').Store
-};
+var Flux = require('./lib/flux.js').Flux,
+  _ = require('lodash');
 
+var flux = new Flux();
+//function Store() {}
+//_.extend(Store.prototype, flux.Store.prototype);
+
+var Store = flux.Store;
+
+var store = new Store();
+
+store.register(function (action) {
+  console.log(action);
+  console.log(this);
+});
+
+flux.dispatch({
+  
+  actionType: 'foo',
+  
+  error: null
+});
+
+module.exports = require('./lib/flux.js');
