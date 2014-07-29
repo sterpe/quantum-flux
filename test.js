d6ffc36a498e568ad6631887a947f6cbf73c8afb
Steve Terpe <sterpe@rubiconproject.com>
2014-07-28 17:45:50 -0700

    fix queueing

---
+++ test.js
@@ -0,0 +1,89 @@
+/*
+ * 
+ * test.js */
+  var Flux = require('./lib/flux.js').Flux;
+  var flux = new Flux(),
+    _ = require('lodash');
+
+  var Store = flux.Store;
+
+  var storeA = new Store({ 
+    foo: "bar",
+    otherFunc: function (x) {
+      console.log('storeA, otherfunc', x);
+      if (x == 42) {
+        flux.dispatch("24");
+        //flux.dispatch("24");
+      }
+    },
+    someFunc: function (x) {
+      console.log('storeA', x);
+      this.unregister();
+      this.register(this.otherFunc);
+    }
+  });
+
+  storeA.register(storeA.someFunc);
+
+  //Later...
+
+  //logs: 'storeA', 'hello, world'
+
+  //change the registered function 
+
+  //unregister a store if you want to take it out of service.
+
+  //Or extend a custom constructor from the Store.prototype...
+
+  var Store1Factory = function () {
+    //very complex store initialization code here...
+  };
+  
+  _.extend(Store1Factory.prototype, Store.prototype);
+  
+  var store1 = new Store1Factory();
+
+  var store2 = new Store();
+ 
+  //Register an anonymous function (will be called with store as `this`);
+  store1.register(function () {
+    var promise = this.waitFor(store2, function (x) {
+      console.log('store1', x);
+    }, function (e) {
+      console.log(e);
+    });
+  });
+
+  store2.register(function (x) {
+    console.log('store2', x);
+  });
+
+
+
+  //logs:
+  // 'store2 responded',
+  // 'store1', '42'
+
+
+  //Stores extend EventEmitter.prototype
+  
+  var storeC = new Store();
+  storeC.register(function (payload) {
+    this.emit('change', payload, payload.foo);
+  });
+
+  storeC.addListener('change', function (foo) {
+    console.log('StoreC: foo was ', foo);
+  });
+
+
+  flux.dispatchForceNextTick('hello, world');
+  flux.dispatchForceNextTick('42');
+  flux.dispatchForceNextTick({
+    foo: "bar"
+  });
+  //logs:
+  // 'foo was bar'
+
+  
+
