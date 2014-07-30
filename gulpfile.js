cb5320184326a0e7467be9f5970af9525fe414cd
Steve Terpe <sterpe@rubiconproject.com>
2014-07-30 12:51:39 -0700

    mocha/phantomjs unit test harness

---
+++ gulpfile.js
@@ -0,0 +1,83 @@
+/*
+ * 
+ * gulpfile.js */
+
+var gulp = require('gulp'),
+  sys = require('sys'),
+  child_process = require('child_process'),
+  fs = require('fs');
+
+gulp.task('mocha-clean', [], function (cb) {
+  "use strict";
+
+  fs.exists('spec/mocha-phantomjs/tests', function (exists) {
+    if (!exists) {
+      fs.mkdir('spec/mocha-phantomjs/tests', function (error) {
+        return error ? cb(error) :
+            fs.unlink('spec/mocha-phantomjs/tests/mocha-bundle.js',
+              function (error) {
+                cb(error);
+              });
+      });
+    } else {
+      fs.unlink('spec/mocha-phantomjs/tests/mocha-bundle.js',
+        function (error) {
+          cb(error);
+        });
+    }
+  });
+});
+gulp.task('mocha-bundle', ['mocha-clean'], function (cb) {
+  "use strict";
+
+  /* 
+   * Can't seem to get browserifying multiple input files working
+   * with gulp in a nice way...neither can anybody else apparently:
+   *
+   * https://github.com/gulpjs/plugins/issues/47
+   *
+   * This solution is a bit of a sledgehammer, but at least it works.
+   */
+
+  var exec = child_process.exec;
+  exec('node node_modules/browserify/bin/cmd.js' +
+    ' spec/tests/*.js' +
+    ' -o spec/mocha-phantomjs/tests/mocha-bundle.js',
+    function (error, stdout, stderr) {
+      sys.print('stdout: ' + stdout + '\n');
+      sys.print('stderr: ' + stderr + '\n');
+      cb(error);
+    });
+
+});
+
+gulp.task('mocha', ['mocha-bundle'], function (cb) {
+  "use strict";
+
+  var spawn = child_process.spawn,
+    address = 'localhost',
+    port = '8081',
+    http,
+    mocha_phantomjs;
+
+  http = spawn('node_modules/http-server/bin/http-server',
+    [ '-a', address, '-p', port], {
+      stdio: "inherit"
+    });
+
+  mocha_phantomjs =
+    spawn('node_modules/mocha-phantomjs/bin/mocha-phantomjs',
+      ['http://' + address + ':' + port + '/spec/mocha-phantomjs'], {
+        stdio: "inherit"
+      });
+
+  mocha_phantomjs.on('close', function (code) {
+    http.kill(code ? 'SIGTERM' : 'SIGHUP');
+  });
+
+  http.on('close', function (code) {
+    cb(code);
+  });
+
+});
+
--- spec/tests/example.js
+++ spec/tests/example.js
@@ -1,14 +1,15 @@
 /*
  * 
- * test.js */
+ * example/test.js */
 
-var assert = require("assert");
+/*jslint sloppy:true */
+/*global describe, it */
 
 describe('Array', function () {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1, 2, 3].indexOf(5));
-      assert.equal(-1, [1, 2, 3].indexOf(0));
+      [1, 2, 3].indexOf(5).should.equal(-1);
+      [1, 2, 3].indexOf(0).should.equal(-1);
     });
   });
 });
