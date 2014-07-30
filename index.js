c4cc2aa1865bbd771844f4bb7c0c0bd2b7beffa2
Steve Terpe <sterpe@rubiconproject.com>
2014-07-29 20:08:38 -0700

    clean up file names etc.

---
+++ index.js
@@ -0,0 +1,33 @@
+/*
+ * 
+ * flux.js */
+var _ = require('lodash'),
+  Dispatcher = require('./lib/dispatcher.js').Dispatcher;
+
+/**
+ * @class
+ * @extends Dispatcher
+ */
+
+function Flux() {
+  "use strict";
+  Dispatcher.prototype.constructor.call(this);
+}
+
+_.extend(Flux.prototype, Dispatcher.prototype);
+
+module.exports = {
+
+  Flux: function () {
+    "use strict";
+    var flux = new Flux(),
+      StoreFactory = _.bind(flux.Store, flux);
+
+    StoreFactory.prototype = flux.Store.prototype;
+
+    return {
+      dispatch: _.bind(flux.dispatch, flux),
+      Store: StoreFactory
+    };
+  }
+};
